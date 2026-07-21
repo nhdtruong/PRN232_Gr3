8c6a08b93610863b296f39a82515aa778498b840
Vietanh1712 <trvietanh4@gmail.com>
2026-07-22 01:28:21 +0700

    bỏ api thừa

--- src/PROJECT_PRN232_.Api/Controllers/AuthController.cs
+++ src/PROJECT_PRN232_.Api/Controllers/AuthController.cs
@@ -22,19 +22,6 @@ namespace PROJECT_PRN232_.Controllers
             _userRepository = userRepository;
         }
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterDto dto)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var result = await _authService.RegisterAsync(dto);
-            if (!result) return BadRequest(new { message = "Tên tài khoản đã tồn tại!" });
-            return Ok(new { message = "Đăng ký tài khoản thành công!" });
-        }
-
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginDto dto)
         {
